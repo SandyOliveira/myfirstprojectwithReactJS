7264f6796012e9a65c0dac9a42100fbffa6743f0
SandyOliveira <sandyoliveira828@gmail.com>
2020-08-01 19:09:36 -0300

    Listagem de repositórios funcionando

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,7 +3,7 @@ import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 
 import api from '../../services/api';
 
-import { Container, Form, SubmitButton } from './styles';
+import { Container, Form, SubmitButton, List } from './styles';
 
 export default class Main extends Component {
     state = {
@@ -36,7 +36,7 @@ export default class Main extends Component {
     };
  
     render() {
-        const { newRepo, loading } = this.state;
+        const { newRepo, repositories, loading } = this.state;
         return (
             <Container>
                 <h1>
@@ -59,6 +59,14 @@ export default class Main extends Component {
                         )}
                     </SubmitButton>
                 </Form>
+                <List>
+                    {repositories.map(repository =>(
+                        <li Key={repository.name}> 
+                            <span>{repository.name}</span>
+                            <a href="">Detalhes</a>
+                        </li>
+                    ))}
+                </List>
             </Container>
         );
   }
